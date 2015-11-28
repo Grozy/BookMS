fec7ce618ae34b8d11756a7364b9bbac47c792f7
SngGrozy <guozhi.sun@yolu-inc.com>
2015-11-29 00:04:54 +0800

    新增model

--- BookMS/BOOKMS/admin.py
+++ BookMS/BOOKMS/admin.py
@@ -1,7 +1,7 @@
 #-*- coding:utf-8 –*-
 
 from django.contrib import admin
-from models import Book,UserBaseInfo,BookBorrow,BorrowHistory
+from models import Book, UserBaseInfo, BookBorrow, BorrowHistory
 import xadmin
 from xadmin import  views
 # Register your models here.
@@ -13,20 +13,24 @@ from xadmin import  views
 #     book_ISBN = models.CharField('ISBN',max_length=13)  #ISBN十三位
 #     book_paper_type = models.CharField('纸质',max_length=10)
 #     book_description = models.TextField('内容简介')
+
 class BookList(admin.ModelAdmin):
     list_display = ("book_name", "book_create_time")
     search_fields = ("book_name", "book_create_time")#搜索
-    list_filter = ('book_create_time',)#时间进行过滤  今天、昨天、本周、本月、本年~
+    list_filter = ('book_create_time',) #时间进行过滤  今天、昨天、本周、本月、本年~
     # filter_horizontal = ('book_name') check box
+
 class UserList(admin.ModelAdmin):
-    list_display = ("user_name", "user_account", "user_password")#列表页显示的数据
+    list_display = ("user_name", "user_account", "user_password") #列表页显示的数据
 
 class BorrowHistoryDisplay(admin.ModelAdmin):
     list_display = ("book", "user", "borrow_time")
-    ordering = ("-borrow_time",)#根据借阅时间排序
+    ordering = ("-borrow_time",) #根据借阅时间排序
 
 admin.site.register(Book, BookList)
 admin.site.register(UserBaseInfo, UserList)
 admin.site.register(BorrowHistory, BorrowHistoryDisplay)
 xadmin.site.register(Book)
 xadmin.site.register(UserBaseInfo)
+xadmin.site.register(BookBorrow)
+xadmin.site.register(BorrowHistory)
